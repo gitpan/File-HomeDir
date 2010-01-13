564fb0c3c5915b5b3b39b494df492fe96c96ca12
Adam Kennedy <adamk@cpan.org>
2010-01-13 14:40:34 +0000

    Import of ADAMK/File-HomeDir-0.90_02 from CPAN.
    gitpan-cpan-distribution: File-HomeDir
    gitpan-cpan-version:      0.90_02
    gitpan-cpan-path:         ADAMK/File-HomeDir-0.90_02.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     developer

--- lib/File/HomeDir.pm
+++ lib/File/HomeDir.pm
@@ -11,7 +11,7 @@ use File::Spec ();
 # Globals
 use vars qw{$VERSION @ISA @EXPORT @EXPORT_OK $IMPLEMENTED_BY};
 BEGIN {
-	$VERSION = '0.90_01';
+	$VERSION = '0.90_02';
 
 	# Inherit manually
 	require Exporter;
@@ -75,7 +75,7 @@ if ( $IMPLEMENTED_BY ) {
 } elsif ( $^O eq 'MacOS' ) {
 	# Legacy Mac OS
 	$IMPLEMENTED_BY = 'File::HomeDir::MacOS9';
-} elsif ( -d '/etc/xdg' ) {
+} elsif ( -d '/etc/xdg' || -d '/usr/local/etc/xdg' ) {
 	# freedesktop unixes
 	$IMPLEMENTED_BY = 'File::HomeDir::FreeDesktop';
 } else {
